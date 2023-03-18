2e418ad5e2504219ca620573512811f15d464ff6
Mariah Gift Miranda <mariah.miranda@novare.com.hk>
2023-03-18 17:49:31 +0800

    change BrowserRoute to HashRoute

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Context } from "./context/Context";
 
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <Context>
       <App />
     </Context>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
